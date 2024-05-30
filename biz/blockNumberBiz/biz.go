fc6074ddc5100a7d578e4afed47eb299a9dd719d
Jason Liu <jasondev310@gmail.com>
2024-05-30 18:00:28 +0800

    sdk struct added

--- biz/blockNumberBiz/biz.go
+++ biz/blockNumberBiz/biz.go
@@ -4,6 +4,7 @@ import (
 	"cfx-evm-sync-sdk/sync/blockNumberSync"
 	"github.com/openweb3/web3go"
 	"github.com/openweb3/web3go/types"
+	"github.com/spf13/viper"
 )
 
 // Support:
@@ -15,43 +16,37 @@ import (
 type GetFunc func(*web3go.Client, uint64) (interface{}, error)
 
 func BlockByNumber(nodes []string, startBlock, endBlock uint64) {
+	// init sdk in sync.gp
+	var result []blockNumberSync.DataWrap
+	s := blockNumberSync.NewSdk(nodes, viper.GetUint64("preload.count"), viper.GetUint64("preload.min"), result)
 	GetFunc := func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
-		return w3client.Eth.BlockByNumber(types.BlockNumber(blockNumber), false)
+		block, err := w3client.Eth.BlockByNumber(types.BlockNumber(blockNumber), false)
+		return block.Number, err
 	}
-	blockNumberSync.InitConcurrentGet(nodes, startBlock, endBlock, GetFunc)
-}
-
-func MultiTask(nodes []string, startBlock, endBlock uint64) {
-	funcs := []GetFunc{
-		func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
-			return w3client.Eth.BlockByNumber(types.BlockNumber(blockNumber), false)
-		},
-		func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
-			blk := types.BlockNumberOrHashWithNumber(types.BlockNumber(blockNumber))
-			return w3client.Eth.BlockReceipts(&blk)
-		},
-		func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
-			return w3client.Eth.BlockTransactionCountByNumber(types.BlockNumber(blockNumber))
-		},
-	}
-
-	blockNumberSync.InitMultiTask(nodes, startBlock, endBlock, funcs)
-}
-
-func BlockReceipts(nodes []string, startBlock, endBlock uint64) {
-	GetFunc := func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
+	s.InitConcurrentGet(startBlock, endBlock, GetFunc)
+	var result02 []blockNumberSync.DataWrap
+	s = blockNumberSync.NewSdk(nodes, viper.GetUint64("preload.count"), viper.GetUint64("preload.min"), result02)
+	GetFunc = func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
 		blk := types.BlockNumberOrHashWithNumber(types.BlockNumber(blockNumber))
 		return w3client.Eth.BlockReceipts(&blk)
 	}
-	blockNumberSync.InitConcurrentGet(nodes, startBlock, endBlock, GetFunc)
+	s.InitConcurrentGet(startBlock, endBlock, GetFunc)
 }
 
-func BlockTransactionCountByNumber(nodes []string, startBlock, endBlock uint64) {
-	GetFunc := func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
-		return w3client.Eth.BlockTransactionCountByNumber(types.BlockNumber(blockNumber))
-	}
-	blockNumberSync.InitConcurrentGet(nodes, startBlock, endBlock, GetFunc)
-}
+//func BlockReceipts(nodes []string, startBlock, endBlock uint64) {
+//	GetFunc := func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
+//		blk := types.BlockNumberOrHashWithNumber(types.BlockNumber(blockNumber))
+//		return w3client.Eth.BlockReceipts(&blk)
+//	}
+//	blockNumberSync.InitConcurrentGet(nodes, startBlock, endBlock, GetFunc)
+//}
+//
+//func BlockTransactionCountByNumber(nodes []string, startBlock, endBlock uint64) {
+//	GetFunc := func(w3client *web3go.Client, blockNumber uint64) (interface{}, error) {
+//		return w3client.Eth.BlockTransactionCountByNumber(types.BlockNumber(blockNumber))
+//	}
+//	blockNumberSync.InitConcurrentGet(nodes, startBlock, endBlock, GetFunc)
+//}
 
 //func BlockUnclesCountByNumber(nodes []string, startBlock, endBlock uint64) {
 //	blockNumberSync.InitConcurrentGet(nodes, startBlock, endBlock, "BlockUnclesCountByNumber")
--- sync/blockNumberSync/sync.go
+++ sync/blockNumberSync/sync.go
@@ -5,7 +5,6 @@ import (
 	"cfx-evm-sync-sdk/rpc"
 	"fmt"
 	"github.com/openweb3/web3go"
-	"github.com/spf13/viper"
 	"log"
 	"sync"
 )
@@ -18,8 +17,25 @@ var poolMutex sync.Mutex
 var dataPool map[uint64]DataWrap
 var nextNum uint64
 
+// Sdk 包含节点信息
+type Sdk struct {
+	Nodes        []string
+	preloadCount uint64
+	preloadMin   uint64
+	Result       []DataWrap
+}
+
 // TODO: 增加一个缓存错误blockNumber用于重发的池子
 // 具体什么时候发送？
+
+func NewSdk(nodes []string, preloadC, preloadM uint64, res []DataWrap) *Sdk {
+	return &Sdk{
+		Nodes:        nodes,
+		preloadCount: preloadC,
+		preloadMin:   preloadM,
+		Result:       res,
+	}
+}
 
 func initDataPool() {
 	dataPool = make(map[uint64]DataWrap)
@@ -39,22 +55,19 @@ func dataTrim(w3client *web3go.Client, blockNumber uint64, nodeUrl string, getFu
 	addDataToPool(blockNumber, result)
 }
 
-func PreloadPool(nodes []string, startBlock uint64, getFunc blockNumberBiz.GetFunc) {
+func (s *Sdk) PreloadPool(startBlock uint64, getFunc blockNumberBiz.GetFunc) {
 	initDataPool()
-	preloadCount := viper.GetUint64("preload.count")
-	concurrentFetchData(nodes, startBlock, preloadCount, getFunc)
+	s.concurrentFetchData(startBlock, s.preloadCount, getFunc)
 }
 
-func concurrentFetchData(nodes []string, startBlock, preloadNewGet uint64, getFunc blockNumberBiz.GetFunc) {
+func (s *Sdk) concurrentFetchData(startBlock, preloadNewGet uint64, getFunc blockNumberBiz.GetFunc) {
 	var wg sync.WaitGroup
-	wg.Add(len(nodes))
-
-	for index, node := range nodes {
+	wg.Add(len(s.Nodes))
+	for index, node := range s.Nodes {
 		go func(nodeUrl string, index int) {
 			defer wg.Done()
 			w3client := rpc.NewClient(nodeUrl)
-
-			blocksPerNode := preloadNewGet / uint64(len(nodes))
+			blocksPerNode := preloadNewGet / uint64(len(s.Nodes))
 			nodeStartBlock := startBlock + (blocksPerNode * uint64(index))
 			var nodeEndBlock uint64
 			if nodeStartBlock+blocksPerNode-1 <= startBlock+preloadNewGet-1 {
@@ -72,17 +85,17 @@ func concurrentFetchData(nodes []string, startBlock, preloadNewGet uint64, getFu
 	nextNum = startBlock + preloadNewGet
 }
 
-func ConcurrentGetPool(nodes []string, startBlock, endBlock uint64, getFunc blockNumberBiz.GetFunc) {
+func (s *Sdk) ConcurrentGetPool(startBlock, endBlock uint64, getFunc blockNumberBiz.GetFunc) {
 
 	var wg sync.WaitGroup
-	wg.Add(len(nodes))
+	wg.Add(len(s.Nodes))
 
 	var fetchLock sync.Mutex
 
-	for index := range nodes {
+	for index := range s.Nodes {
 		go func(index int) {
 			defer wg.Done()
-			blocksPerNode := (endBlock - startBlock + 1) / uint64(len(nodes))
+			blocksPerNode := (endBlock - startBlock + 1) / uint64(len(s.Nodes))
 			nodeStartBlock := startBlock + (blocksPerNode * uint64(index))
 			var nodeEndBlock uint64
 			if nodeStartBlock+blocksPerNode-1 <= endBlock {
@@ -94,18 +107,15 @@ func ConcurrentGetPool(nodes []string, startBlock, endBlock uint64, getFunc bloc
 			for blockNumber := nodeStartBlock; blockNumber <= nodeEndBlock; blockNumber++ {
 				poolMutex.Lock()
 				// data, ok := dataPool[blockNumber]
-				_, ok := dataPool[blockNumber]
+				data, ok := dataPool[blockNumber]
 				if ok {
 					delete(dataPool, blockNumber) // 从池中删除已取出的数据
 				}
-				// poolMutex.Unlock()
-				// TODO: 每个收到的data要具体做什么操作？
-				// fetchLock.Lock()
+				// 输出的result
+				s.Result = append(s.Result, data)
 				// 每扣一个都检测剩余池子还能不能满足30个，如果不满足，先填满再接着扣
-				if uint64(len(dataPool)) < viper.GetUint64("preload.min") {
-					preloadCount := viper.GetUint64("preload.count")
-					preloadMin := viper.GetUint64("preload.min")
-					concurrentFetchData(nodes, nextNum, preloadCount-preloadMin, getFunc)
+				if uint64(len(dataPool)) < s.preloadMin {
+					s.concurrentFetchData(nextNum, s.preloadCount-s.preloadMin, getFunc)
 				}
 				fmt.Printf("现在pool长度是： %d.\n", len(dataPool))
 				fetchLock.Unlock()
@@ -116,32 +126,24 @@ func ConcurrentGetPool(nodes []string, startBlock, endBlock uint64, getFunc bloc
 	wg.Wait()
 }
 
-func InitConcurrentGet(nodes []string, startBlock, endBlock uint64, getFunc blockNumberBiz.GetFunc) {
+func (s *Sdk) InitConcurrentGet(startBlock, endBlock uint64, getFunc blockNumberBiz.GetFunc) {
 
 	// 预加载池中的数据
-	PreloadPool(nodes, startBlock, getFunc)
+	s.PreloadPool(startBlock, getFunc)
 
 	start := startBlock
-	end := viper.GetUint64("preload.count")
+	end := s.preloadCount
 	if endBlock < end {
 		end = endBlock
 	}
 
 	for start <= endBlock {
-		ConcurrentGetPool(nodes, start, end, getFunc)
+		s.ConcurrentGetPool(start, end, getFunc)
 		start = end + 1
-		end = end + viper.GetUint64("preload.count")
+		end = end + s.preloadCount
 		if endBlock < end {
 			end = endBlock
 		}
 
 	}
-}
-
-func InitMultiTask(nodes []string, startBlock, endBlock uint64, getFuncs []blockNumberBiz.GetFunc) {
-	if len(getFuncs) < len(nodes) {
-		log.Printf("Functions exceeds node limits")
-		return
-	}
-
 }
